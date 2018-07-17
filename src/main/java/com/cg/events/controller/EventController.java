34389b8ab53984f243dcbff68dab39de52423fc1
Mandeep <mspanesar145@gmail.com>
2018-07-17 15:35:47 +0530

    added chnages for deletng notifications on deleting event

--- src/main/java/com/cg/events/controller/EventController.java
+++ src/main/java/com/cg/events/controller/EventController.java
@@ -210,6 +210,7 @@ public class EventController {
 			} else {
 				eventService.deleteGathering(gatehringId);
 			}
+			notificationManager.deleteNotificationByNotificationTypeId(event.getEventId());
 			deleteResponse.put("success",true);
 			deleteResponse.put("message","Gathering deleted Successfully");
 			deleteResponse.put("errorCode",0);
