d42b63540d7e422695bbec1d80b2df00caa14db5
Mandeep <mspanesar145@gmail.com>
2018-08-11 22:39:52 +0530

    added changes for fixing query

--- src/main/java/com/cg/repository/NotificationCountDataRepository.java
+++ src/main/java/com/cg/repository/NotificationCountDataRepository.java
@@ -15,6 +15,6 @@ public interface NotificationCountDataRepository extends JpaRepository<Notificat
 	NotificationCountData findByUserId(Long userId);
 	
 	@Modifying
-    @Query("UPDATE NotificationCountData ncd SET ncd.badgeCount = 0 WHERE badgeCount.userId = :userId")
+    @Query("UPDATE NotificationCountData ncd SET ncd.badgeCount = 0 WHERE ncd.userId = :userId")
     int setBadgeCountsToZero(@Param("userId") Long userId);
 }
