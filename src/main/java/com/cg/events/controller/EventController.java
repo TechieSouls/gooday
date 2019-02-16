f02c0f8addac1aa80c0ea19e4822f81a2ede8eae
cenes_dev <cenes_dev@192.168.1.3>
2019-02-16 19:27:10 +0800

    Added code to save event image thumbnail

--- src/main/java/com/cg/events/controller/EventController.java
+++ src/main/java/com/cg/events/controller/EventController.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -17,12 +19,18 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.UUID;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import okhttp3.internal.framed.ErrorCode;
@@ -854,6 +862,41 @@ public class EventController {
 
 			String eventImageUrl = domain + "/assets/uploads/events/large/" + fileName;
 			event.setEventPicture(eventImageUrl);
+			
+		    BufferedImage image = ImageIO.read(newFile);
+
+		    String thumbnailPath = eventUploadPath+"thumbnail/"+fileName;
+		    File thumbnailPathFile = new File(thumbnailPath);
+			if (!thumbnailPathFile.exists()) {
+				try {
+					thumbnailPathFile.mkdirs();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		    File compressedImageFile = new File(thumbnailPath);
+		    OutputStream os = new FileOutputStream(compressedImageFile);
+
+		    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(extension);
+		    ImageWriter writer = (ImageWriter) writers.next();
+
+		    ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+		    writer.setOutput(ios);
+
+		    ImageWriteParam param = writer.getDefaultWriteParam();
+
+		    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+		    param.setCompressionQuality(0.01f);  // Change the quality value you prefer
+		    writer.write(null, new IIOImage(image, null, null), param);
+
+		    os.close();
+		    ios.close();
+		    writer.dispose();
+			
+		    String thumbnailImageUrl = domain + "/assets/uploads/events/thumbnail/" + fileName;
+			event.setEventPicture(thumbnailImageUrl);
+			
 			event = eventManager.saveUpdateEvent(event);
 			
 			JSONObject jobj = new JSONObject();
