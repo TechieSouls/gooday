30ed4904ae3df3d99324c483223257836bf452cc
cenes_dev <cenes_dev@192.168.1.8>
2019-02-20 11:35:00 +0800

    Added changes for email

--- src/main/java/com/cg/manager/EmailManager.java
+++ src/main/java/com/cg/manager/EmailManager.java
@@ -28,13 +28,7 @@ public class EmailManager {
 	    	  MimeMessage message = new MimeMessage(session);    
 	           message.addRecipient(Message.RecipientType.TO,new InternetAddress(user.getEmail()));    
 	           message.setSubject("Reset your password");    
-	           
-	           StringBuffer sb = new StringBuffer();
-	           sb.append("Hi "+user.getName()+",").append("\n");
-	           sb.append("We received a request to reset the password for your account. Please click on the link below or paste this into your browser to complete the process. This link is valid for one hour.").append("\n");
-	           sb.append("http://ec2-18-216-7-227.us-east-2.compute.amazonaws.com/reset-password.html?resetToken="+user.getResetToken()).append("\n");
-	           sb.append("If you did not ask to change your password, please ignore this email and your account will remain unchanged.").append("\n");
-	           message.setText(sb.toString());    
+	           message.setContent(resetPasswordHTMLTemplate(user.getResetToken()), "text/html");   
 	           //send message  
 	           Transport.send(message);    
 	           System.out.println("message sent successfully");    
@@ -62,4 +56,64 @@ public class EmailManager {
 		});
 	}
 	
+	public String resetPasswordHTMLTemplate(String resetPasswordToken) {
+		String resetPasswordLink = "https://www.cenesgroup.com/app/forgetPassword?resetToken="+resetPasswordToken;
+		String htmlContent = "<html>\n" + 
+				"<body style=\"font-size: 18px;font-family: AvenirLTStd Book;color: #595757;\">\n" + 
+				"	\n" + 
+				"<div align=\"center\"><img src=\"https://www.cenesgroup.com/assets/images/Logo.png\"></div>\n" + 
+				"<div align=\"center\">\n" + 
+				"	\n" + 
+				"	<h3 style=\"letter-spacing: 1px;font-size: 28px;font-weight: lighter;\">\n" + 
+				"		Reset Password\n" + 
+				"	</h3>\n" + 
+				"</div>\n" + 
+				"<br/>\n" + 
+				"<div align=\"center\">Hi Louisa,</div>\n" + 
+				"<br>\n" + 
+				"<br>\n" + 
+				"<div align=\"center\">You have requested a link to reset password. Click the link below to complete your request:</div>\n" + 
+				"<br/>\n" + 
+				"<br/>\n" + 
+				"<br/>\n" + 
+				"\n" + 
+				"<div align=\"center\">\n" + 
+				"	<a href=\""+resetPasswordLink+"\" style=\"text-decoration: none;color: #EE9B26;\"><span style=\"font-size: 22px;border:2px solid #EE9B26; padding:25px 100px; border-radius: 40px;\">Reset Password</span></a>\n" + 
+				"</div>\n" + 
+				"<br>\n" + 
+				"<br>\n" + 
+				"<br>\n" + 
+				"<div align=\"center\">If you didn't request this, please ignore this email.</div>\n" + 
+				"<br>\n" + 
+				"\n" + 
+				"<div align=\"center\">Your account remains the same until you access the link above and reset your password.</div>\n" + 
+				"<br>\n" + 
+				"<br>\n" + 
+				"<div align=\"center\">&#169; CENES</div>\n" + 
+				"</body>\n" + 
+				"<style type=\"text/css\">\n" + 
+				"\n" + 
+				" @font-face {\n" + 
+				"    font-family: 'AvenirLTStd Book';\n" + 
+				"    src: url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.eot');\n" + 
+				"    src: url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.eot?#iefix') format('embedded-opentype'),\n" + 
+				"        url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.woff2') format('woff2'),\n" + 
+				"        url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.woff') format('woff'),\n" + 
+				"        url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.ttf') format('truetype'),\n" + 
+				"        url('https://www.cenesgroup.com/assets/fonts/AvenirLTStd-Book.svg#AvenirLTStd-Book') format('svg');\n" + 
+				"    font-weight: normal;\n" + 
+				"    font-style: normal;\n" + 
+				"}\n" + 
+				"  </style>\n" + 
+				"</html>\n" + 
+				"";
+		
+		return htmlContent;
+	}
+	
+	/*public static void main(String[] args) {
+		User user = new User();
+		user.setEmail("mspanesar145@gmail.com");
+		new EmailManager().sendForgotPasswordLink(user);
+	}*/
 }
