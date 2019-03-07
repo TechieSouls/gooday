3c1b6868d9b533f66441e66decd67a3e2dac5eb9
cenes_dev <cenes_dev@192.168.1.9>
2019-03-07 18:16:40 +0800

    Chanegd query to fetch 100 events

--- src/main/java/com/cg/events/dao/EventServiceDao.java
+++ src/main/java/com/cg/events/dao/EventServiceDao.java
@@ -110,10 +110,16 @@ public class EventServiceDao {
 	
 	public List<Event> findByCreatedByIdAndStartDateAndEventMemberStatus(Long createdById, String eventDate, String endDate) {
 		
-		String query = "select *, event_temp.source as event_source,  em.source as member_source, em.name as non_cenes_member_name, u.name as origname from (select e.* from events e JOIN event_members em on e.event_id = em.event_id where "
+		/*String query = "select *, event_temp.source as event_source,  em.source as member_source, em.name as non_cenes_member_name, u.name as origname from (select e.* from events e JOIN event_members em on e.event_id = em.event_id where "
 				+ "e.start_time >= '"+eventDate+"' and e.start_time <=  '"+endDate+"' and  em.user_id = "+createdById+" and em.status = 'Going' "
 				+ "and e.schedule_as in ('Event','Holiday','Gathering')) as event_temp JOIN event_members em on event_temp.event_id = em.event_id "
-				+ "LEFT JOIN users u on em.user_id = u.user_id order by event_temp.start_time asc";
+				+ "LEFT JOIN users u on em.user_id = u.user_id order by event_temp.start_time asc";*/
+		
+		
+		String query = "select *, event_temp.source as event_source,  em.source as member_source, em.name as non_cenes_member_name, u.name as origname from (select e.* from events e JOIN event_members em on e.event_id = em.event_id where "
+				+ "e.start_time >= '"+eventDate+"' and  em.user_id = "+createdById+" and em.status = 'Going' "
+				+ "and e.schedule_as in ('Event','Holiday','Gathering')) as event_temp JOIN event_members em on event_temp.event_id = em.event_id "
+				+ "LEFT JOIN users u on em.user_id = u.user_id order by event_temp.start_time asc limit 100";
 	
 		System.out.println("Home Events Query : "+query);
 		List<Map<String, Object>> userGatheringsMapList = jdbcTemplate.queryForList(query);
