328fa6cca82efda918366b88652b4a293f5f863f
cenes_dev <techiesoulsin@gmail.com>
2019-05-02 14:24:16 +0800

    Added contact list name for event members in gatherings query

--- src/main/java/com/cg/dao/EventServiceDao.java
+++ src/main/java/com/cg/dao/EventServiceDao.java
@@ -207,12 +207,14 @@ public class EventServiceDao {
 	}
 	
 	
-	public List<Event> findPageableEventsByCreatedByIdAndStartDate(Long createdById, String eventDate, int offset) {
+	public List<Event> findPageableEventsByCreatedByIdAndStartDate(Long userId, String eventDate, int offset) {
 	
-		String query = "select *, event_temp.source as event_source,  em.source as member_source, em.name as non_cenes_member_name, u.name as origname from (select e.* from events e JOIN event_members em on e.event_id = em.event_id where "
-				+ "e.start_time >= '"+eventDate+"' and  em.user_id = "+createdById+" and em.status = 'Going' "
+		String query = "select *, event_temp.source as event_source,  em.source as member_source, em.name as non_cenes_member_name, "
+				+ "u.name as origname, uc.name as phonebookName from (select e.* from events e JOIN event_members em on e.event_id = em.event_id where "
+				+ "e.start_time >= '"+eventDate+"' and  em.user_id = "+userId+" and em.status = 'Going' "
 				+ "and e.schedule_as in ('Event','Holiday','Gathering')) as event_temp JOIN event_members em on event_temp.event_id = em.event_id "
-				+ "LEFT JOIN users u on em.user_id = u.user_id order by event_temp.start_time asc limit "+offset+", 50";
+				+ "LEFT JOIN users u on em.user_id = u.user_id LEFT JOIN user_contacts uc on em.user_id = uc.friend_id and uc.user_id = "+userId+" "
+				+ "order by event_temp.start_time asc limit "+offset+", 50";
 	
 		System.out.println("Home Events Query : "+query);
 		List<Map<String, Object>> userGatheringsMapList = jdbcTemplate.queryForList(query);
