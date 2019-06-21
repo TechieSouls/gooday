cab8f98042baf98199158feb14a415b36c2a7c99
cenes_dev <techiesoulsin@gmail.com>
2019-06-21 20:02:36 +0800

    Added logging

--- src/main/java/com/cg/service/GoogleService.java
+++ src/main/java/com/cg/service/GoogleService.java
@@ -208,12 +208,14 @@ public class GoogleService {
 
 		
 		String recurringEventsAPI = "https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=100&singleEvents=true&timeMin="+URLEncoder.encode(sdf.format(minTime))+"&alt=json";
+		System.out.println("Url recurringEventsAPI : "+recurringEventsAPI);
 		JSONObject calResponse = httpService.getRequestWithAuthorization(recurringEventsAPI, "GET", accessToken);//doGoogleCalendarRestRequest(recurringEventsAPI,"GET");
 		googleCalendarEvents.addAll(parseGoogleEventsResponse(calResponse,true));
 		
 		httpService = new HttpService();
 		//String normalEventsAPI = events_list_api_str+"?key="+CenesUtils.googleAPIKey+"&future_events=true"+tokenParam+"&timeMin="+URLEncoder.encode(sdf.format(minTimeCal.getTime()))+"&timeMax="+URLEncoder.encode(sdf.format(maxTimeCal.getTime()));
 		String normalEventsAPI = "https://www.googleapis.com/calendar/v3/calendars/primary/events?maxResults=100&timeMin="+URLEncoder.encode(sdf.format(minTime))+"&alt=json";
+		System.out.println("Url normalEventsAPI : "+recurringEventsAPI);
 		calResponse = httpService.getRequestWithAuthorization(normalEventsAPI, "GET", accessToken);//doGoogleCalendarRestRequest(normalEventsAPI,"GET");
 		googleCalendarEvents.addAll(parseGoogleEventsResponse(calResponse,false));
 		
